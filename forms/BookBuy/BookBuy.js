a85c30dfb505de8b3bb1ff0dd755cba239dc3dd9
Connor <cmg58665@gmail.com>
2019-11-24 20:17:01 -0600

    6

--- forms/BookBuy/BookBuy.js
+++ forms/BookBuy/BookBuy.js
@@ -1,3 +1,12 @@
+itemTitle = ""
+itemCondition = ""
+itemPrice = ""
+itemPublisher = ""
+itemAuthor = "" 
+itemGenre = ""
+itemDescription = ""
+
+
 BookBuy.onshow=function(){
     var query = "SELECT * FROM books WHERE Title =" + '"' + lookupTitle + '"' 
    // alert(query)
@@ -5,5 +14,28 @@ BookBuy.onshow=function(){
     if (req1.status == 200) { //transit worked.
         results = JSON.parse(req1.responseText)
         console.log(results)
+        itemTitle = results[0][1]
+        itemCondition = results[0][2]
+        itemPrice = results[0][5]
+        itemPublisher = results[0][8]
+        itemAuthor = results[0][6]
+        itemGenre = results[0][7]
+        itemDescription = results[0][3]
+        console.log(itemDescription)
+
+        //Populate Form
+        taBuyDescription.value  = itemDescription
+        inptBuyCondition.value = itemCondition
+        inptBuyAuthor.value = itemAuthor
+        inptBuyGenre.value = itemGenre
+        inptBuyPublisher.value = itemPublisher
+        lblBuyTitle.value = itemTitle
+        inptBuyPrice.value = itemPrice
+        
         }
+}
+
+
+btnBuyBookBack.onclick=function(){
+  ChangeForm(BooksSearch);
 }
